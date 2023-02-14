fc6b572afcdc855fa88899597b0328b1c13675a4
Irwan X Yans <irwan080304@gmail.com>
2023-02-14 14:53:46 +0700

    Create Latihan_1.pas

---
+++ Februari/14_feb/Latihan_1.pas
@@ -0,0 +1,11 @@
+uses Crt ;
+var panjang, lebar, luas : Real;
+begin 
+write('Nilai Panjang : ');Readln(panjang);
+write('Nilai Lebar : ');ReadLn(lebar);
+luas := Panjang*Lebar;
+WriteLn('Luas Persegi Panjang : ', luas:2:0, 'M2');
+if  luas<=100 then write('Tanah Cukup Luas')
+else if luas<=300 then write('Tanah Luas')
+else write('Tanah Luas Sekali');
+end.
