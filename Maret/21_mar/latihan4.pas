9cd98f54554a636a083093500e467e250181d040
Irwan X Yans <51849384+irwanx@users.noreply.github.com>
2023-03-23 15:47:59 +0700

    Update latihan4.pas

--- Maret/21_mar/latihan4.pas
+++ Maret/21_mar/latihan4.pas
@@ -1,16 +1,23 @@
 uses
    crt;
-
+{
+* DEKLARASI
+}
 var
    no_debitur, ulang, tanggal, nama_debitur, kode_jenis_pinjaman, kondisi, anu, sikon, yt : string;
    jumlah_pinjaman, angsuran_pokok, jml_angsuran_perbulan, jangka_waktu, bunga_sebulan, hasil_bunga, jump : real;
    debitur_ke : integer ;
 begin
+{
+* LOOP WHILE DO ( Y/N )
+}
    ulang:='y';
    while ((ulang='Y') or (ulang='y')) do
 begin
 clrscr();
-{------Blok Tampilan------}
+{
+* BLOK TAMPILAN
+}
 gotoxy(10,5);write('BPR Syariah KONARYOSIH');
 gotoxy(10,6);write('Jl. Magelang-Jogja Km. 13');
 gotoxy(10,7);write('Telp. 0293-234 123, Fax. 0293-890 765');;
@@ -29,13 +36,20 @@ gotoxy(52,15);write('Bunga/Bulan  : RP.');
 gotoxy(10,16);write('Jumlah Ngsuran/Bln    : Rp.');
 gotoxy(10,17);write('------------------------------------------------------------------------');
 
-{------Blok Input------}
+{
+* BLOCK INPUT
+}
 
 gotoxy(34,9);readln(no_debitur);
 gotoxy(65,9);readln(tanggal);
 gotoxy(34,10);readln(nama_debitur);
+{
+* AUTO INCREMENT
+}
 inc(debitur_ke);gotoxy(65, 10); write(debitur_ke);
-
+{ 
+* START LOOP 1 REPEAT UNTIL
+}
 repeat
 gotoxy(34,11);readln(kode_jenis_pinjaman);
 if kode_jenis_pinjaman='001' then
@@ -73,7 +87,10 @@ else
      gotoxy(34,11);write('    '); 
    end;
 until kondisi='F';
-{ Batas Loop 1 }
+{ 
+* END LOOP 1 REPEAT UNTIL
+* STARR LOOP 2 WHILE DO
+}
 while sikon<>'F' do
 begin
 gotoxy(34,14);readln(jumlah_pinjaman);
@@ -90,7 +107,10 @@ else
      gotoxy(34,14);write('          '); 
    end;
 end;
-
+{ 
+* END LOOP 2 WHILE DO
+* START LOOP 3 REPEAT UNTIL
+}
 repeat
 gotoxy(67,14);readln(jangka_waktu);
 if ((jangka_waktu<=48) and (jangka_waktu >0)) then
@@ -110,6 +130,9 @@ else
      gotoxy(67,14);write('    '); 
    end;
 until anu='F';
+{
+* END LOOP 3 REPEAT UNTIL
+}
 gotoxy(20,18); write(' Apakah ada data lagi [Y/T] : '); readln(ulang);
 end;
 end.
